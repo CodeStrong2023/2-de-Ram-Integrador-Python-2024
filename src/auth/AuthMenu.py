765340e730f11cc340f7a9a820d4537dfed9a3cd
Luis Mera <luismera86@gmail.com>
2024-05-13 17:35:34 -0300

    Agregado el menú principal y conectado con el registro de usuario

--- src/auth/AuthMenu.py
+++ src/auth/AuthMenu.py
@@ -1,4 +1,5 @@
 from src.utils.InputUtils import InputUtils
+from src.User.UserServices import UserServices
 class auth_menu:
     option = 0
     def __init__(self):
@@ -7,16 +8,18 @@ class auth_menu:
             "2 - Register",
             "3 - Exit"
         ]
-
     def display_menu(self):
         for item in self.auth_menu:
             print(item)
         self.option = InputUtils.int_input("Ingrese una opción: ")
         if self.option == 1:
             print("Ingrese su mail")
-        else:
-            print("Opción no válida")
-            self.option = InputUtils.int_input("Ingrese una opción: ")
+        elif self.option == 2:
+            register_menu = UserServices()
+            register_menu.create_user()
+        elif self.option == 3:
+            print("Gracias por su visita")
+
     
 
  
