fa8f00f921180200042a76e1f10e205a995abc70
ArtiedaLorena <artiedalorena@gmail.com>
2024-05-16 18:43:40 -0300

    Creación de PetService y modificacion de PetModel

--- src/Pet/PetModel.py
+++ src/Pet/PetModel.py
@@ -2,11 +2,12 @@ class Pet:
 
     howMany = 0
 
-    def __init__(self, name, species, breed, age, color, hair, size, coat, energy, sociability):
+    def __init__(self, name, species, breed, age, gender, color, hair, size, coat, energy, sociability):
         self.name = name  # nombre
         self.species = species  # especie
         self.breed = breed  # raza
         self.age = age  # edad
+        self.gender = gender #género
         self.color = color
         self.hair = hair  # tipo de pelo: con manchas, atigrado, etc
         self.size = size  # tamaño: grande, pequeño, etc
@@ -23,6 +24,7 @@ class Pet:
         Especie: {self.species}
         Raza: {self.breed}
         Edad: {self.age}
+        Género:{self.gender}
         Color: {self.color}
         Tamaño: {self.size}
         Dibujo del pelo: {self.hair}
