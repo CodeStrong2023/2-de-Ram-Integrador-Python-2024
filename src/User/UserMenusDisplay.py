5947a6bbab4c5db1c72809a6bddd04bceb82acc5
Luis Mera <luismera86@gmail.com>
2024-05-28 17:05:28 -0300

    Modificaciones y revisión de código

--- src/User/UserMenusDisplay.py
+++ src/User/UserMenusDisplay.py
@@ -15,4 +15,14 @@ class UserMenusDisplay:
         print("=" * 20)
         print("")
 
-UserMenusDisplay().display_user({})
+    def user_requirements(self):
+        print(" Requisitos para adoptar una mascota: ")
+        print(f'''
+                1- Ser mayor de edad.
+                2- Adjuntar fotocopias del DNI, del derecho y el reverso. 
+                3- Completar formulario de adopción.
+                3- Tiempo, compromiso y dedicación. Asegúrate de estar dispuesto a cuidarla y brindarle atención.
+                4- Ser una persona o familia responsable que los quiera como a un miembro más, los proteja y cuide.
+            ''')
+
+# UserMenusDisplay().display_user({})
