8f0a19a9094adae4d84b305da3e7b3e3fe7d0960
Luis Mera <luismera86@gmail.com>
2024-06-03 22:03:05 -0300

    Se re hizo el login y se adapto la session para que tome los datos del usuario logueado

---
+++ src/auth/Login.py
@@ -0,0 +1,31 @@
+from src.User.UserServices import UserServices
+from src.admin.AdminMenu import AdminMenu
+from src.User.UserMenu import UserMenu
+from src.utils.InputUtils import InputUtils
+from src.auth.SessionUser import SessionUser
+
+
+class Login:
+    user_services = UserServices()
+    user_menu = UserMenu()
+    admin_menu = AdminMenu()
+
+    @staticmethod
+    def login():
+        email = InputUtils.email_input("Ingrese su email: ")
+        password = InputUtils.str_input("Ingrese su password: ")
+        user = Login.user_services.get_user_by_email(email)
+        print(user)
+        # Chequeamos que exista el usuario, que el password coincida y que su estatus esté en True
+        if not user or user["password"] != password or user["status"] == False:
+            print("Usuairo o contraseña no válido")
+            return Login.login()
+
+        # Guardamos la sessión de usuario para usar en toda la app
+        SessionUser.user_session = user
+
+        # Determinamos de acuerdo al rol del usuario lo reenviamos al menú correspondiente
+        if user["role"] == "admin":
+            Login.admin_menu.display_menu()
+        else:
+            Login.user_menu.display_menu()
--- src/auth/SessionUser.py
+++ src/auth/SessionUser.py
@@ -1,19 +1,13 @@
 class SessionUser:
     user_session = None
 
-    def __init__(self, id, user_name, role, email):
-        self.user_session = {
-            "id": id,
-            "name": user_name,
-            "role": role,
-            "email": email
+    @staticmethod
+    def set_user_session(user):
+        user = {
+            "id": user["_id"],
+            "name": user["name"],
+            "email": user["email"],
+            "role": user["role"],
+            "pets": user["pets"],
         }
-
-    # @property
-    # def get_user_session(self):
-    #     user_session = {
-    #         "name": self._user_name,
-    #         "role": self._role,
-    #         "email": self._email
-    #     }
-    #     return user_session
+        SessionUser.user_session = user
